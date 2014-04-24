8635b3c19c7260973dfb707ed336d33248d016c4
jinjianxin <jinjianxinjin@gmail.com>
2014-04-24 13:17:33 +0800

    add TestBox2d

---
+++ TestBox2d/Classes/HelloWorldScene.h
@@ -0,0 +1,33 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+#include "Box2D/Box2D.h"
+#define PTM_RATIO 32.0
+
+USING_NS_CC;
+
+class HelloWorld : public cocos2d::CCLayer
+{
+public:
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual ~HelloWorld();
+    virtual bool init();  
+
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::CCScene* scene();
+    
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+    
+    // implement the "static node()" method manually
+    CREATE_FUNC(HelloWorld);
+
+	CCSprite *ball;
+	b2World *world;
+	b2Body *body;
+
+	void tick(float dt);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
